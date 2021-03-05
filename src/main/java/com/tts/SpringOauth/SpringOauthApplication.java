8666eae7eb3674ce53ff5f52f9b231cf796562d2
Michael Brave <michaelbrave@outlook.com>
2021-03-05 01:42:51 -0600

    initial commit

---
+++ src/main/java/com/tts/SpringOauth/SpringOauthApplication.java
@@ -0,0 +1,13 @@
+package com.tts.SpringOauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringOauthApplication.class, args);
+	}
+
+}
